71745fa3c6d5e32fb1bb679fdfe048252253bc4c
ekhiw <ekkyhiw@gmail.com>
2017-08-02 13:21:09 +0700

    nambah Log di detail activity

--- app/src/main/java/id/yuktanding/animechart/Detail.java
+++ app/src/main/java/id/yuktanding/animechart/Detail.java
@@ -5,13 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class Detail extends AppCompatActivity {
 
+    private static final String TAG = "android debug =>";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"activity detail onCreate");
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
