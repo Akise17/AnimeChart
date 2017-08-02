d53ced1b44ffd28c9a48c9d4c8757e6308f05a36
ekhiw <ekkyhiw@gmail.com>
2017-08-02 13:45:01 +0700

    Tambah class Item_Adapter

---
+++ app/src/main/java/id/yuktanding/animechart/Item_Adapter.java
@@ -0,0 +1,12 @@
+package id.yuktanding.animechart;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ekkyh on 8/2/2017.
+ */
+
+public class Item_Adapter extends RecyclerView<Item_Adapter>.MyviewHolder
+{
+
+}
