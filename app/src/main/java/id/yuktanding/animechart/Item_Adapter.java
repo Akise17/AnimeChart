3141ae3072e76b63415156db627774050547c060
ekhiw <ekkyhiw@gmail.com>
2017-08-02 13:50:13 +0700

    Tambah class Item_Adapter dan variable

--- app/src/main/java/id/yuktanding/animechart/Item_Adapter.java
+++ app/src/main/java/id/yuktanding/animechart/Item_Adapter.java
@@ -1,12 +1,22 @@
 package id.yuktanding.animechart;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ekkyh on 8/2/2017.
  */
 
-public class Item_Adapter extends RecyclerView<Item_Adapter>.MyviewHolder
+public class Item_Adapter extends RecyclerView<Item_Adapter.MyviewHolder>
 {
+    private static final String TAG = "Android Debug";
+    public static final String JUDUL = "JUDUL";
+    public static final String SINOPSIS = "SINOPSIS";
+    public static final int RATING = 0 ;
+
+    private ArrayList<item> items;
+    private Context context;
 
 }
