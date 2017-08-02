7a6dbe7261041903a8476a8852bc4eb708bec986
Akise17 <ahmad.fauzan1603@gmail.com>
2017-08-02 13:40:47 +0700

    Nambah Item

---
+++ app/src/main/java/id/yuktanding/animechart/Item.java
@@ -0,0 +1,81 @@
+package id.yuktanding.animechart;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.widget.ImageView;
+
+/**
+ * Created by Akise on 02/08/2017.
+ */
+
+public class Item implements Parcelable{
+    private String judulAnime, ratingAnime, sinopsisAnime;
+    private int imgAnime;
+
+    public String getJudulAnime() {
+        return judulAnime;
+    }
+
+    public void setJudulAnime(String judulAnime) {
+        this.judulAnime = judulAnime;
+    }
+
+    public String getRatingAnime() {
+        return ratingAnime;
+    }
+
+    public void setRatingAnime(String ratingAnime) {
+        this.ratingAnime = ratingAnime;
+    }
+
+    public String getSinopsisAnime() {
+        return sinopsisAnime;
+    }
+
+    public void setSinopsisAnime(String sinopsisAnime) {
+        this.sinopsisAnime = sinopsisAnime;
+    }
+
+    public int getImgAnime() {
+        return imgAnime;
+    }
+
+    public void setImgAnime(int imgAnime) {
+        this.imgAnime = imgAnime;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.judulAnime);
+        dest.writeString(this.ratingAnime);
+        dest.writeString(this.sinopsisAnime);
+        dest.writeInt(this.imgAnime);
+    }
+
+    public Item() {
+    }
+
+    protected Item(Parcel in) {
+        this.judulAnime = in.readString();
+        this.ratingAnime = in.readString();
+        this.sinopsisAnime = in.readString();
+        this.imgAnime = in.readInt();
+    }
+
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel source) {
+            return new Item(source);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+}
