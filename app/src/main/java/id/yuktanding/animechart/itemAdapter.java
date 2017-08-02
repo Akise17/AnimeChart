b94dc6b273317ced8425e930fb64b55dc7ebce00
ekhiw <ekkyhiw@gmail.com>
2017-08-02 13:59:50 +0700

    Update Item Adapter

--- app/src/main/java/id/yuktanding/animechart/itemAdapter.java
+++ app/src/main/java/id/yuktanding/animechart/itemAdapter.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,6 +15,10 @@ import java.util.ArrayList;
  * Created by ekkyh on 8/2/2017.
  */
 
+/*  Step Ketujuh RecyclerView: Buat Class Adapter untuk RecyclerView*/
+/*  1. Buat Class ItemAdapter*/
+
+/*  2. extends RecyclerView.Adapter<NamaAdapter.NamaViewHolder>*/
 public class itemAdapter extends RecyclerView.Adapter<itemAdapter.MyviewHolder>
 {
     private static final String TAG = "Android Debug";
@@ -19,6 +26,7 @@ public class itemAdapter extends RecyclerView.Adapter<itemAdapter.MyviewHolder>
     public static final String SINOPSIS = "SINOPSIS";
     public static final int RATING = 0 ;
 
+    /*  3. Buat Variabel untuk menyimpan data dari activity utama*/
     private ArrayList<Item> items;
     private Context context;
 
@@ -41,5 +49,14 @@ public class itemAdapter extends RecyclerView.Adapter<itemAdapter.MyviewHolder>
     public itemAdapter(ArrayList<Item> items, Context context) {
         this.items = items;
         this.context = context;
+    }
+
+    public class MyviewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View view){
+            super(view);
+            JUDUL = (TextView) view.findViewById(R.id.);
+            SINOPSIS = (TextView) view.findViewById(R.id.l);
+        }
     }
 }
